4f3f5501eba20fb4e3380b3e38f4afa00d9b3e29
fahadmaq <fahadmem2007@gmail.com>
2022-12-20 10:05:40 +0530

    12-20

---
+++ SampleCode/AnagStr.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode
+{
+    class AnagStr
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first string");
+            string firstString = Console.ReadLine();
+
+            Console.WriteLine("Enter Second string");
+            string secondString = Console.ReadLine();
+
+            if (firstString.Length==secondString.Length)
+            {
+                char[] ch1 = (firstString.ToLower()).ToCharArray();
+                char[] ch2 = (secondString.ToLower()).ToCharArray();
+
+                Array.Sort(ch1);
+                Array.Sort(ch2);
+
+                String word1 = new string(ch1);
+                String word2 = new string(ch2);
+
+                if (word1==word2)
+                {
+                    Console.WriteLine("Strngs are anagram",firstString,secondString);
+                }
+                else
+                {
+                    Console.WriteLine("Strings are not anagram");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Strings are not  anagram",firstString,secondString);
+            }
+        }
+    }
+
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            int i, count, n1 = 0, n2 = 1, n3;
+            Console.WriteLine("Enter upper limit");
+            count = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("----Fiboacci Series for Upper Limit------");
+
+            Console.WriteLine(n1);
+            //Console.WriteLine(n2);
+
+            for(i=0;i<=count;i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3);
+
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
---
+++ SampleCode/MinMaxArr.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode
+{
+    class MinMaxArr
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 12, 23, 56, 4, 74, 63, 41 };
+            int min = 0, max = 0;
+            for (int i = 0; i < arr.Length; i++)
+
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if(arr[i]>arr[j])
+                    {
+                        max = arr[i];
+                    }
+                    else if(arr[i]<arr[j])
+
+                        {
+                        min = arr[i];
+
+
+                        }
+
+                }
+
+            }
+
+            Console.WriteLine($"min-{min},  max-{max}");
+
+
+
+        }
+
+
+    }
+}
