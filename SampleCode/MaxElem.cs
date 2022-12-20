b91565768f28e1dcbd588d39b84038ce3277d0f8
fahadmaq <fahadmem2007@gmail.com>
2022-12-20 10:48:58 +0530

    3rd

---
+++ SampleCode/MaxElem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleCode
+{
+    class MaxElem
+    {
+        public  void FindElem(int[,]s)
+        {
+            for (int i=0;i<s.GetLength(0);i++)
+            {
+                int maxmum = s[0, 1];
+                for (int j = 0; j < s.GetLength(1); j++)
+                {
+                    if(s[i,j]>maxmum)
+                    {
+                        maxmum = s[i, j];
+                    }
+                }
+                Console.WriteLine(maxmum);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] arr =
+            {
+                {4,8,87,6 },
+                {2,7,35,4 },
+                {6,8,2,100 },
+                {4,9,56,12 }
+            };
+            MaxElem N = new MaxElem();
+            N.FindElem(arr);
+        }
+
+    }
+}
